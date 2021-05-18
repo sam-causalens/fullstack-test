1cb9ea83272e0a3a7fdb2df6048e15cf3c918de6
Sam Smith <sam@causalens.com>
2021-05-18 10:18:55 +0100

    Initial Commit: Add utilities for running a model and returning model stats and predictions

---
+++ utils.js
@@ -0,0 +1,33 @@
+
+function runModel(data) {
+    const keys = Object.keys(data[0]);
+
+    if (target === 'index' || !(target in keys)) {
+        throw new Error(`Invalid target: ${target} selected for model`);
+    }
+
+    const predictions = data.map(item => ({ index: item.index, prediction: item[target] *  (Math.random() + 0.5) }));
+    return {
+        modelSummary: {
+            algoType: "linear.OLS",
+            nRetrain: 0,
+            nTrainingGap: 0,
+            nTrainingWarmup: 0,
+            nWindowSize: -1,
+            scaling: "no_scaling",
+            trainingMode: "offline",
+            weightDecay: 0.0,
+        },
+        predictions,
+        scoring_metrics: {
+            pearsonR: Math.random(),
+            spearmanRho: Math.random(),
+            meanAbsError: Math.random() * 2,
+            medianAbsError: Math.random() * 2,
+            percentageAbsError: Math.random() * 100,
+            signMatch: Math.random(),
+        },
+    };
+}
+
+module.exports = runModel
---
+++ utils.ts
@@ -0,0 +1,59 @@
+type Data = Array<{ [k: string]: number } & { index: string }>;
+
+interface Prediction {
+    index: string;
+    prediction: number;
+}
+
+interface ModelResponse {
+    modelSummary: {
+        algoType: string;
+        nRetrain: number;
+        nTrainingGap: number;
+        nTrainingWarmup: number;
+        nWindowSize: number;
+        scaling: string;
+        trainingMode: string;
+        weightDecay: number;
+    };
+    predictions: Array<Prediction>;
+    scoringMetrics: {
+        pearsonR: number;
+        spearmanRho: number;
+        meanAbsError: number;
+        medianAbsError: number;
+        percentageAbsError: number;
+        signMatch: number;
+    }
+}
+
+export default function runModel(data: Data, target: string): ModelResponse {
+    const keys = Object.keys(data[0]);
+
+    if (target === 'index' || !(target in keys)) {
+        throw new Error(`Invalid target: ${target} selected for model`);
+    }
+
+    const predictions = data.map(item => ({ index: item.index, prediction: item[target] *  (Math.random() + 0.5) }));
+    return {
+        modelSummary: {
+            algoType: "linear.OLS",
+            nRetrain: 0,
+            nTrainingGap: 0,
+            nTrainingWarmup: 0,
+            nWindowSize: -1,
+            scaling: "no_scaling",
+            trainingMode: "offline",
+            weightDecay: 0.0,
+        },
+        predictions,
+        scoringMetrics: {
+            pearsonR: Math.random(),
+            spearmanRho: Math.random(),
+            meanAbsError: Math.random() * 2,
+            medianAbsError: Math.random() * 2,
+            percentageAbsError: Math.random() * 100,
+            signMatch: Math.random(),
+        },
+    };
+}
